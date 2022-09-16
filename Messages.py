bd76190cb407d3d235d2c3aef7df97a3f5433500
Moaaz-Rafique <moaazrafiquetk@gmail.com>
2022-09-16 21:25:42 +0500

    add new email

--- Messages.py
+++ Messages.py
@@ -1,9 +1,13 @@
+from ctypes import sizeof
 import imaplib
 import email
 import os
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+import sys
+from memory_profiler import profile
 load_dotenv()
+
 
 # account credentials
 username = os.getenv("EMAIL")
@@ -51,11 +55,11 @@ def clean(text):
     return "".join(c if c.isalnum() else "_" for c in text)
 
 # number of top emails to fetch
-N = 1
+N = 10000
 
 # create an IMAP4 class with SSL, use your email provider's IMAP server
 
-
+@profile
 def getMessages():
     # select a mailbox (in this case, the inbox mailbox)
     imap = imaplib.IMAP4_SSL(imap_server)
@@ -79,74 +83,87 @@ def getMessages():
 
     print("No of Messages: ",messages)
     records = []
-    for i in range(messages, 0, -1):    
+    for i in range(messages, messages-N, -1):    
         res, msg = imap.fetch(str(i), '(BODY.PEEK[])')
-        print("Getting messages no: ",str(i))
+        print("Getting messages no: ",messages-i)
         
         recordText = ""
 
-        for response in msg:                
-            if isinstance(response, tuple):            
-                record=dict()
-                msg = email.message_from_bytes(response[1])   
-                for j in columns:
-                    if isinstance(msg[j], str):         
-                        
-                        record[j.replace("-", "")] = msg[j]
-                    elif msg[j]:
-                        record[j.replace("-", "")] = msg[j].as_string()
+        for response in msg:  
+            try:              
+                if isinstance(response, tuple):            
+                    record=dict()
+                    msg = email.message_from_bytes(response[1])   
+                    for j in columns:
+                        if isinstance(msg[j], str):         
+                            
+                            record[j.replace("-", "")] = msg[j]
+                        elif msg[j]:
+                            record[j.replace("-", "")] = msg[j].as_string()
+                        else:
+                            record[j.replace("-", "")] = ""
+                        # print(j.replace("-", "")+": ", record[j.replace("-", "")])
+                    if msg.is_multipart():
+                        for part in msg.walk():
+                            content_type = part.get_content_type()
+                            content_disposition = str(part.get("Content-Disposition"))
+                            try:
+                                body = part.get_payload(decode=True)
+                            except Exception as e:
+                                print(e)
+                                pass
+                            if content_type == "text/plain" and "attachment" not in content_disposition:
+                                ("printing part ")
+                            elif "attachment" in content_disposition:
+                                recordText+=part.get_payload(decode=True)
+                            elif content_type == "text/html":
+                                soup = BeautifulSoup(part.get_payload(), features="html.parser")
+                                for script in soup(["script", "style"]):
+                                    script.extract()
+                                text = soup.get_text()                            
+                                lines = (line.strip() for line in text.splitlines())                            
+                                chunks = (phrase.strip() for line in lines for phrase in line.split("  "))                            
+                                text = '\n'.join(chunk for chunk in chunks if chunk)                            
+                                recordText+=text
+                            elif  content_type == "text/plain":
+                                recordText+=part.get_payload(decode=True)
+                        # print("="*100)            
                     else:
-                        record[j.replace("-", "")] = ""
-                    # print(j.replace("-", "")+": ", record[j.replace("-", "")])
-                if msg.is_multipart():
-                    for part in msg.walk():
-                        content_type = part.get_content_type()
-                        content_disposition = str(part.get("Content-Disposition"))
-                        try:
-                            body = part.get_payload(decode=True)
-                        except Exception as e:
-                            print(e)
-                            pass
-                        if content_type == "text/plain" and "attachment" not in content_disposition:
-                            ("printing part ")
-                        elif "attachment" in content_disposition:
-                            recordText+=part.get_payload()
-                        elif content_type == "text/html":
-                            soup = BeautifulSoup(part.get_payload(), features="html.parser")
+                        try:            
+                            body = msg.get_payload(decode=True)
+                            soup = BeautifulSoup(body, features="html.parser")
                             for script in soup(["script", "style"]):
                                 script.extract()
                             text = soup.get_text()                            
                             lines = (line.strip() for line in text.splitlines())                            
                             chunks = (phrase.strip() for line in lines for phrase in line.split("  "))                            
-                            text = '\n'.join(chunk for chunk in chunks if chunk)                            
+                            text = '\n'.join(chunk for chunk in chunks if chunk)
                             recordText+=text
-                        elif  content_type == "text/plain":
-                            recordText+=part.get_payload()
-                    # print("="*100)            
-                else:
-                    try:            
-                        body = msg.get_payload(decode=True).decode()
-                        soup = BeautifulSoup(body, features="html.parser")
-                        for script in soup(["script", "style"]):
-                            script.extract()
-                        text = soup.get_text()                            
-                        lines = (line.strip() for line in text.splitlines())                            
-                        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))                            
-                        text = '\n'.join(chunk for chunk in chunks if chunk)
-                        recordText+=text
-                    except Exception as e:
-                        print(e)        
-                # print(recordText)
-                record['Time'] = record["Date"][16:25]
-                record['Day'] = record["Date"][0:3]
-                record['ShortDate'] = record["Date"][5:16]
-                record["MessageText"] = recordText
-                records.append(record)
+                        except Exception as e:
+                            print(e)        
+                    # print(recordText)
+                    record['Time'] = record["Date"][16:25]
+                    record['Day'] = record["Date"][0:3]
+                    record['ShortDate'] = record["Date"][5:16]
+                    record["MessageText"] = recordText
+                    records.append(record)
+            except imap.abort:
+                # imap.login(username, password)
+                print("Imap is aborted")
+                break
+            except Exception as e:
+                print(e)    
+                # imap.login(username, password)
+                print(sys.getsizeof(records))
             # print("="*100)
 
     imap.close()
     imap.logout()
+    print("Total size",sys.getsizeof(records))
     print("Got all the Messages")
     return records
-# (getMessages())
+# try:
+#     (getMessages())
+# except Exception as e:
+#     print(e)
 # print(getMessages)
--- app.py
+++ app.py
@@ -34,31 +34,21 @@ def loadEmails():
         adjustedColumns = [i.replace("-", "") for i in columns]
         adjustedColumns.append("MessageText")
         print(adjustedColumns)
-
-
         records = getMessages()
-
-
         dataframe = pandas.DataFrame(
                 records,
                 columns=adjustedColumns,
                 # index=[i['MessageID'] for i in records]
         )
-
             # print(dataframe.columns)
-
             # dataframe.to_csv("sheesh.csv")
-
-
         job_config = bigquery.LoadJobConfig(
                 write_disposition="WRITE_TRUNCATE",
             )
-
         job = client.load_table_from_dataframe(    
                 dataframe, table_id,job_config=job_config
         )  # Make an API request.
         job.result()  # Wait for the job to complete.
-
         table = client.get_table(table_id)  # Make an API request.
         print(
                 "Loaded {} rows and {} columns to {}".format(
@@ -69,10 +59,13 @@ def loadEmails():
     except Exception as e:
         print(e)
 
-while True:
-    loadEmails()
-    print("Emails are loaded")
-    for i in range(1 * 3 * 60 ,0,-1):
-        print(f"{int(i/60)}minutes and {i%60} seconds")
-        time.sleep(1)
+
+loadEmails()
+
+# while True:
+#     loadEmails()
+#     print("Emails are loaded")
+#     for i in range(1 * 60 * 60 ,0,-1):
+#         print(f"{int(i/60)} minutes and {i%60} seconds")
+#         time.sleep(1)
     
